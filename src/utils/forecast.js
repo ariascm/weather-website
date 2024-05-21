1298b3247883cd828b2b06a1a31cd9dcd1dd89e8
CRISTIAN ARIAS <arias_mza@hotmail.com>
2024-05-20 21:48:35 -0300

    Added about description and Humidity in forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,17 +6,17 @@ const request = require('request')
 
 const forecast = (lat, lon, callback) => {
     const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=1472f72dbb0d310ac5828ed656cfca2f&units=metric'
-
+console.log({lat, lon})
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect with to weather service', undefined)
         } else if (body.cod != 200) {
             callback('Incorrect Latitude/Longitude', undefined)
         } else {
-            const { temp, feels_like } = body.main
+            const { temp, feels_like, humidity } = body.main
             const { description } = body.weather[0]
 
-            callback(undefined, description + '. The temperature is ' + temp + ' and the feels like sensation ' + feels_like)
+            callback(undefined, description + '. The temperature is ' + temp + ' and the feels like sensation ' + feels_like + '. The Humidity is ' + humidity )
         }
     })
 }
